1657d5573f6e13ad633eff9d2b78c1e3c13d8cbf
borisdvlpr <eduffmiranda@hotmail.com>
2024-03-11 00:34:48 +0100

    refactor: changed port binding for parallel execution

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use zero2prod_newsletter::run;
 
+use std::net::TcpListener;
+
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind port.");
+    run(listener)?.await
 }
