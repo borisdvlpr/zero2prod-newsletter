0b445877ee19fb2ce6d193c6a328065837d56396
borisdvlpr <eduffmiranda@hotmail.com>
2024-04-04 20:04:35 +0200

    feat: added data persistency on subscriptions endpoint
    fix: replaced PgConnection with PgPool avoiding concurrency errors and limitations

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,5 +1,7 @@
 use actix_web::{web, HttpResponse};
-use sqlx::PgConnection;
+use sqlx::PgPool;
+use uuid::Uuid;
+use chrono::Utc;
 
 #[derive(serde::Deserialize)]
 #[allow(dead_code)]
@@ -9,8 +11,24 @@ pub struct FormData {
 }
 
 pub async fn subscribe(
-    _form: web::Form<FormData>,
-    _connection: web::Data<PgConnection>,
+    form: web::Form<FormData>,
+    pool: web::Data<PgPool>,
 ) -> HttpResponse {
-    HttpResponse::Ok().finish()
+    match sqlx::query!(r#"
+        INSERT INTO subscriptions (id, email, name, subscribed_at)
+        VALUES($1, $2, $3, $4)
+        "#,
+        Uuid::new_v4(),
+        form.email,
+        form.name,
+        Utc::now()
+    )
+    .execute(pool.get_ref())
+    .await {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(e) => {
+            println!("Failed to execute query: {}", e);
+            HttpResponse::InternalServerError().finish()
+        }
+    }
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,18 +1,18 @@
 use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
-use sqlx::PgConnection;
+use sqlx::PgPool;
 use std::net::TcpListener;
 
-pub fn run(listener: TcpListener, connection: PgConnection) -> Result<Server, std::io::Error> {
+pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
     // wrap the connection in a smart pointer (Arc, giving each instance of the app a pointer to the connection)
-    let connection = web::Data::new(connection);
+    let db_pool = web::Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             // register the connection as part of the application state
-            .app_data(connection.clone())
+            .app_data(db_pool.clone())
     })
     .listen(listener)?
     .run();
