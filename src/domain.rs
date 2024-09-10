ef3815f34dd9f222ceeb3561ec6504033096c650
borisdvlpr <eduffmiranda@hotmail.com>
2024-09-10 23:57:14 +0200

    test: added unit tests to domain to test parsing user name function

--- src/domain.rs
+++ src/domain.rs
@@ -19,10 +19,10 @@ impl SubscriberName {
     pub fn parse(s: String) -> Result<SubscriberName, String> {
         let is_empty_or_whitespace = s.trim().is_empty();
 
-        // a graphmeme is defined by the unicode standard as a "user-perceived"
-        // character: 'æ' is a single graphmeme, but it is composed of two characters, 'a' and 'e'
-        // and it returns an iterator over the graphmemes of the input 's'.
-        // 'true' specifies that we want to use the extended graphmeme definition set,
+        // a grapheme is defined by the unicode standard as a "user-perceived"
+        // character: 'æ' is a single grapheme, but it is composed of two characters, 'a' and 'e'
+        // and it returns an iterator over the graphemes of the input 's'.
+        // 'true' specifies that we want to use the extended grapheme definition set,
         // the recommended one
         let is_too_long = s.graphemes(true).count() > 256;
 
@@ -38,4 +38,46 @@ impl SubscriberName {
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use crate::domain::SubscriberName;
+    use claims::{assert_err, assert_ok};
 
+    #[test]
+    fn a_256_grapheme_long_name_is_valid() {
+        let name = "ë".repeat(256);
+        assert_ok!(SubscriberName::parse(name));
+    }
+
+    #[test]
+    fn a_name_longer_than_256_graphemes_is_rejected() {
+        let name = "a".repeat(257);
+        assert_err!(SubscriberName::parse(name));
+    }
+
+    #[test]
+    fn whitespace_only_names_are_rejected() {
+        let name = " ".to_string();
+        assert_err!(SubscriberName::parse(name));
+    }
+
+    #[test]
+    fn empty_string_is_regected() {
+        let name = "".to_string();
+        assert_err!(SubscriberName::parse(name));
+    }
+
+    #[test]
+    fn names_containing_an_invalid_character_are_rejected() {
+        for name in &['/', '(', ')', '"', '<', '>', '\\', '{', '}'] {
+            let name = name.to_string();
+            assert_err!(SubscriberName::parse(name));
+        }
+    }
+
+    #[test]
+    fn a_valid_name_is_parsed_successfully() {
+        let name = "Ursula Le Guin".to_string();
+        assert_ok!(SubscriberName::parse(name));
+    }
+}
