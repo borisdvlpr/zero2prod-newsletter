61d227426a254ce0a3eee0201aee9fbe6ef5505a
borisdvlpr <eduffmiranda@hotmail.com>
2024-09-23 02:06:38 +0200

    test: initial unit test setup for email client

--- src/email_client.rs
+++ src/email_client.rs
@@ -26,3 +26,42 @@ impl EmailClient {
         todo!();
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::domain::SubscriberEmail;
+    use crate::email_client::EmailClient;
+    use fake::faker::internet::en::SafeEmail;
+    use fake::faker::internet::raw::SafeEmail;
+    use fake::faker::lorem::raw::{Paragraph, Sentence};
+    use fake::Fake;
+    use wiremock::matchers::any;
+    use wiremock::{Mock, MockServer, ResponseTemplate};
+
+    #[tokio::test]
+    async fn send_email_fires_a_request_to_base_url() {
+        // http server
+        let mock_server = MockServer::start().await;
+        let sender = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let email_client = EmailClient::new(mock_server.uri(), sender);
+
+        // mounting a 'Mock' changes the behaviour according to the given condition
+        Mock::given(any())
+            .respond_with(ResponseTemplate::new(200))
+            // should expect 1 request that matches the 'Mock'
+            .expect(1)
+            .mount(&mock_server)
+            .await;
+
+        let subscriber_email = SubscriberEmail::parse(SafeEmail.fake()).unwrap();
+        let subject: String = Sentence(1..2).fake();
+        let content: String = Paragraph(1..2).fake();
+
+        // act
+        let _ = email_client
+            .send_email(subscriber_email, &subject, &content, &content)
+            .await;
+
+        // assert
+    }
+}
