ccf47cb80685dba218caa5c5d026afe020dbf3da
borisdvlpr <eduffmiranda@hotmail.com>
2024-10-10 18:14:37 +0200

    test: added unit test for validating unsuccessful email send

--- src/email_client.rs
+++ src/email_client.rs
@@ -64,9 +64,9 @@ impl EmailClient {
 
 #[cfg(test)]
 mod tests {
-    use claims::assert_ok;
     use crate::domain::SubscriberEmail;
     use crate::email_client::EmailClient;
+    use claims::{assert_err, assert_ok};
     use fake::faker::internet::en::SafeEmail;
     use fake::faker::lorem::en::{Paragraph, Sentence};
     use fake::{Fake, Faker};
@@ -142,9 +142,33 @@ mod tests {
             .mount(&mock_server)
             .await;
 
-        let outcome = email_client.send_email(subscriber_email, &subject, &content, &content)
+        let outcome = email_client
+            .send_email(subscriber_email, &subject, &content, &content)
             .await;
 
-        assert_ok!(outcome)
+        assert_ok!(outcome);
+    }
+
+    #[tokio::test]
+    async fn send_email_fails_if_server_returns_500() {
+        let mock_server = MockServer::start().await;
+        let sender = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let email_client = EmailClient::new(mock_server.uri(), sender, Secret::new(Faker.fake()));
+
+        let subscriber_email = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let subject: String = Sentence(1..2).fake();
+        let content: String = Paragraph(1..2).fake();
+
+        Mock::given(any())
+            .respond_with(ResponseTemplate::new(500))
+            .expect(1)
+            .mount(&mock_server)
+            .await;
+
+        let outcome = email_client
+            .send_email(subscriber_email, &subject, &content, &content)
+            .await;
+
+        assert_err!(outcome);
     }
 }
