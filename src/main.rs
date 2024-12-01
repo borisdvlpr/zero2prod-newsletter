02a18d978b71dbf4daf0406f0022f3b1894f68b7
borisdvlpr <eduffmiranda@hotmail.com>
2024-12-01 14:38:37 +0100

    refactor(NOT WORKING): remove build logic from main file
    This refactor intends to use the splitted logic into the testing setup to avoid code duplication

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::io;
 use std::net::TcpListener;
 use zero2prod_newsletter::configuration::get_configuration;
 use zero2prod_newsletter::email_client::EmailClient;
-use zero2prod_newsletter::startup::run;
+use zero2prod_newsletter::startup::build;
 use zero2prod_newsletter::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
@@ -12,26 +12,8 @@ async fn main() -> Result<(), std::io::Error> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
+    let server = build(configuration).await?;
+    server.await?;
 
-    let connection_pool = PgPoolOptions::new().connect_lazy_with(configuration.database.with_db());
-    let address = format!(
-        "{}:{}",
-        configuration.application.host, configuration.application.port
-    );
-
-    // build an 'EmailClient' using 'configuration'
-    let sender_email = configuration
-        .email_client
-        .sender()
-        .expect("Invalid sender email address");
-    let timeout = configuration.email_client.timeout();
-    let email_client = EmailClient::new(
-        configuration.email_client.base_url,
-        sender_email,
-        configuration.email_client.authorization_token,
-        timeout,
-    );
-
-    let listener = TcpListener::bind(address)?;
-    run(listener, connection_pool, email_client)?.await
+    Ok(())
 }
