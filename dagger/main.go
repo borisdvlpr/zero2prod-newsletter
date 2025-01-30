7032dbcc25cc4fef316413c52222ef08f9bad8c9
borisdvlpr <eduffmiranda@hotmail.com>
2025-01-30 16:46:39 +0100

    build: add base image function

--- dagger/main.go
+++ dagger/main.go
@@ -1,37 +1,66 @@
-// A generated module for Dagger functions
-//
-// This module has been generated via dagger init and serves as a reference to
-// basic module structure as you get started with Dagger.
-//
-// Two functions have been pre-created. You can modify, delete, or add to them,
-// as needed. They demonstrate usage of arguments and return types using simple
-// echo and grep commands. The functions can be called from the dagger CLI or
-// from one of the SDKs.
-//
-// The first line in this comment block is a short description line and the
-// rest is a long description with more detail on the module's purpose or usage,
-// if appropriate. All modules should have a short description.
-
 package main
 
 import (
-	"context"
+	"fmt"
+
 	"dagger/dagger/internal/dagger"
 )
 
-type Dagger struct{}
-
-// Returns a container that echoes whatever string argument is provided
-func (m *Dagger) ContainerEcho(stringArg string) *dagger.Container {
-	return dag.Container().From("alpine:latest").WithExec([]string{"echo", stringArg})
+// Constructor
+func New(
+	source *dagger.Directory,
+) *Dagger {
+	return &Dagger{
+		Source:           source,
+		SqlxVersion:      "0.7.4",
+		SqlxFeatures:     "rustls,postgres",
+		DatabaseHost:     "postgres",
+		DatabaseUser:     "postgres",
+		DatabasePassword: "password",
+		DatabaseName:     "newsletter",
+	}
 }
 
-// Returns lines that match a pattern in the files of the provided Directory
-func (m *Dagger) GrepDir(ctx context.Context, directoryArg *dagger.Directory, pattern string) (string, error) {
-	return dag.Container().
-		From("alpine:latest").
-		WithMountedDirectory("/mnt", directoryArg).
-		WithWorkdir("/mnt").
-		WithExec([]string{"grep", "-R", pattern, "."}).
-		Stdout(ctx)
+type Dagger struct {
+	Source           *dagger.Directory
+	SqlxVersion      string
+	SqlxFeatures     string
+	DatabaseHost     string
+	DatabaseUser     string
+	DatabasePassword string
+	DatabaseName     string
+}
+
+// Build a ready-to-use development environment
+func (m *Dagger) BuildEnv() *dagger.Container {
+	// setup database container
+	postgres := dag.Container().From("postgres:16-bookworm").
+		WithEnvVariable("POSTGRES_PASSWORD", m.DatabasePassword).
+		WithExposedPort(5432).
+		AsService(dagger.ContainerAsServiceOpts{UseEntrypoint: true})
+
+	// format version and features to use constants
+	sqlxVersion := fmt.Sprintf("--version=%s", m.SqlxVersion)
+	sqlxFeatures := fmt.Sprintf("--features=%s", m.SqlxFeatures)
+
+	// create base container
+	return dag.Container().From("rust:slim-bookworm").
+		WithServiceBinding(m.DatabaseHost, postgres).
+		WithDirectory("/hello-rust", m.Source).
+		WithWorkdir("/hello-rust").
+		WithMountedCache("/target", dag.CacheVolume("rust-target")).
+		//WithEnvVariable("CARGO_TERM_COLOR", CARGO_TERM_COLOR).
+		WithEnvVariable("SQLX_VERSION", m.SqlxVersion).
+		WithEnvVariable("SQLX_FEATURES", m.SqlxFeatures).
+		WithEnvVariable("POSTGRES_HOST", m.DatabaseHost).
+		WithEnvVariable("POSTGRES_USER", m.DatabaseUser).
+		WithEnvVariable("POSTGRES_PASSWORD", m.DatabasePassword).
+		WithEnvVariable("POSTGRES_DB", m.DatabaseName).
+		WithEnvVariable("DATABASE_URL", "postgres://postgres:password@postgres:5432/newsletter").
+		WithEnvVariable("APP_ENVIRONMENT", "ci").
+		WithEnvVariable("SKIP_DOCKER", "true").
+		WithExec([]string{"apt-get", "update"}).
+		WithExec([]string{"apt-get", "install", "lld", "clang", "postgresql-client", "-y"}).
+		WithExec([]string{"cargo", "install", "sqlx-cli", sqlxVersion, sqlxFeatures, "--no-default-features", "--locked"}).
+		WithExec([]string{"./scripts/init_db.sh"})
 }
