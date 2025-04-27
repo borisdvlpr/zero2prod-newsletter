b7ac662b0126dd306b46c7d111d85966308acd5b
borisdvlpr <eduffmiranda@hotmail.com>
2025-04-27 03:11:32 +0200

    fix(tests): setup mock to fix subscribe_returns_a_200_for_valid_form_data

--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -4,10 +4,16 @@ use wiremock::{Mock, ResponseTemplate};
 use crate::helpers::spawn_app;
 
 #[tokio::test]
-async fn subscribe_returns_a_200_for_valid_form() {
+async fn subscribe_returns_a_200_for_valid_form_data() {
     let app = spawn_app().await;
-
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .mount(&app.email_server)
+        .await;
+
     let response = app.post_subscription(body.into()).await;
 
     assert_eq!(200, response.status().as_u16());
