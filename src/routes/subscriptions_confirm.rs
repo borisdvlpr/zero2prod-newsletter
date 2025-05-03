26801f20402bd6abfea10d970c60be0f944b9da2
borisdvlpr <eduffmiranda@hotmail.com>
2025-05-03 15:50:45 +0200

    test(subscriptions): add test to verify confirmation link functionality

--- src/routes/subscriptions_confirm.rs
+++ src/routes/subscriptions_confirm.rs
@@ -2,13 +2,10 @@ use actix_web::{web, HttpResponse};
 
 #[derive(serde::Deserialize)]
 pub struct Parameters {
-    subscription_token: String
+    subscription_token: String,
 }
 
-#[tracing::instrument(
-    name = "Confirm a pending subscriber.",
-    skip(_parameters)
-)]
+#[tracing::instrument(name = "Confirm a pending subscriber.", skip(_parameters))]
 pub async fn confirm(_parameters: web::Query<Parameters>) -> HttpResponse {
     HttpResponse::Ok().finish()
 }
